830f061cf42da5974509ffc21d21266843fcce42
Hard Horse <hardhorse1994@yandex.by>
2020-06-27 15:05:37 +0300

    add sleep

--- src/pages/Prices/index.js
+++ src/pages/Prices/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react";
+import rp from "request-promise-native";
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
 import TextField from '@material-ui/core/TextField';
@@ -25,72 +26,85 @@ const useStyles = makeStyles({
   },
 });
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function fetchData(numbers, urlFuncion, cb) {
+  const data = []
+  for (let i = 0; i < numbers.length; i++) {
+    const item = numbers[i];
+    const options = {
+      method: "GET",
+      url: urlFuncion(item),
+      json: true,
+    };
+    const body = await rp(options);
+    console.log(`Awaiting 2s ...`);
+    //waiting before sleep function executes using it synchronously
+    await sleep(2000);
+    data.push(body);
+    cb(body, item);
+  }
+
+  return data;
+}
+
 const Prices = () => {
   const [numbers, setNumbers] = useState("");
   const [isReady, setIsReady] = useState(false);
   const [inProgress, setInProgress] = useState(false);
   const classes = useStyles();
-  const productItems = useRef({});
   const table = useRef(null);
+  const productItems = useRef({});
 
   const handleTextareaChange = e => {
     setNumbers(e.target.value);
   };
 
-  const handleGetResults = () => {
+  const handleGetResults = async () => {
 
     setIsReady(false);
     setInProgress(true);
     const splittedNumbers = numbers.split(/\n/);
 
     if(splittedNumbers.length >= 1) {
-      const shopData = splittedNumbers.map(item =>
-        fetch(getApiUrl(item))
-          .then(res => res.json())
-          .then(data => {
-            if (data.positions) {
-              const availableShops = getShops(data.positions.primary);
+      await fetchData(splittedNumbers, getApiUrl, (data, item) => {
+        if (data.positions) {
+          const availableShops = getShops(data.positions.primary);
 
-              productItems.current[item] = { ...productItems.current[item], ...availableShops};
-            }
-          })
-      );
-      const detailsData = splittedNumbers.map(item =>
-        fetch(getDetailsUrl(item))
-          .then(res => res.json())
-          .then(data => {
-            productItems.current[item] = {
-                ...productItems.current[item],
-                ...getProductData(data)
-              }
-          })
-      );
-      Promise.all(
-        [...shopData, ...detailsData]
-      ).then(() => {
-        const productData = splittedNumbers.map(item => {
-            const product = productItems.current[item];
-
-            if (!product) {
-                return [...SHOPS.map(() => null)]
-            }
-            
-            return [
-                ...getParametersList(isTablet(product))
-                    .map(item =>product[item] && product[item].value),
-                ...SHOPS.map(shop => getProductPrice(product[shop]))
-            ]
-          }).sort(item => item[1] === TABLET_TYPE ? 1 : -1);
-
-        const notebooks = productData.filter(item => item[1] !== TABLET_TYPE);
-        const tablets = productData.filter(item => item[1] === TABLET_TYPE);
-
-
-        table.current = [createTable(notebooks, LAPTOP_TYPE), createTable(tablets, TABLET_TYPE)].filter(Boolean);
-
-        setIsReady(true);
-        setInProgress(false);
+          productItems.current[item] = { ...productItems.current[item], ...availableShops};
+        }
       });
+      await fetchData(splittedNumbers, getDetailsUrl, (data, item) => {
+        productItems.current[item] = {
+          ...productItems.current[item],
+          ...getProductData(data)
+        }
+      });
+
+      const productData = splittedNumbers.map(item => {
+          const product = productItems.current[item];
+
+          if (!product) {
+              return [...SHOPS.map(() => null)]
+          }
+          
+          return [
+              ...getParametersList(isTablet(product))
+                  .map(item =>product[item] && product[item].value),
+              ...SHOPS.map(shop => getProductPrice(product[shop]))
+          ]
+        }).sort(item => item[1] === TABLET_TYPE ? 1 : -1);
+
+      const notebooks = productData.filter(item => item[1] !== TABLET_TYPE);
+      const tablets = productData.filter(item => item[1] === TABLET_TYPE);
+
+
+      table.current = [createTable(notebooks, LAPTOP_TYPE), createTable(tablets, TABLET_TYPE)].filter(Boolean);
+
+      setIsReady(true);
+      setInProgress(false);
     }
   };
 
