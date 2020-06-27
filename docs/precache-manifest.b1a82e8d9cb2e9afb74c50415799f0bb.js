e455466bd535b8fff216740f1d8987bc3408a5e8
Hard Horse <hardhorse1994@yandex.by>
2020-06-27 14:29:10 +0300

    add dcos folder

---
+++ docs/precache-manifest.b1a82e8d9cb2e9afb74c50415799f0bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d14485d081e13a74fac745b29c6e85",
+    "url": "/report-parser/index.html"
+  },
+  {
+    "revision": "fe8f9ef80a6bc475e0d2",
+    "url": "/report-parser/static/css/main.e7c88046.chunk.css"
+  },
+  {
+    "revision": "7e3ca9cf93e977147ab5",
+    "url": "/report-parser/static/js/2.2034a969.chunk.js"
+  },
+  {
+    "revision": "fe8f9ef80a6bc475e0d2",
+    "url": "/report-parser/static/js/main.56ae4243.chunk.js"
+  },
+  {
+    "revision": "aa7745fcd6d5e53caf88",
+    "url": "/report-parser/static/js/runtime~main.3b5c0b56.js"
+  }
+]);
